f9e2415ea7087d607454dc784730d3407cc6663c
OsbornDelAngel <ozdelangel@gmail.com>
2021-10-06 03:57:50 -0700

    start of lab-06

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
 
-function App() {
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      cityName: ''
+    }
+  }
+  getPlace = () => {
+    console.log('button Pressed');
+    let apiKey = 'pk.f2c50aa80b0fb6ac48bb87cff3cde9d4';
+    let URL =  `https://us1.locationiq.com/v1/search.php?key=${apiKey}&q=${this.state.cityName}&format=json`;
+    console.log(URL);
+    let placeData = axios.get(URL);
+    console.log(placeData);
+  }
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1> Proof Of Life</h1>
+    <h3>{this.state.cityName}</h3>
+    <input onChange={(e) => this.setState({cityName: e.target.value})}>
+    </input>
+    <button onClick={this.getPlace}>Search</button>
+    </>
+    
   );
+}
 }
 
 export default App;
